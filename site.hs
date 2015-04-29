6c74858ac4c08e576724b5f1fd1eb6613c2ca875
Steven Shaw <steven@steshaw.org>
2015-04-29 20:24:51 +1000

    Reformat code and fix a few linter nits

--- site.hs
+++ site.hs
@@ -9,76 +9,74 @@ import Control.Applicative
 main :: IO ()
 main = hakyll $ do
 
-    match "source/**" $ do
-        route   $ gsubRoute "source/" (const "")
-        compile copyFileCompiler
+  match "source/**" $ do
+    route   $ gsubRoute "source/" (const "")
+    compile copyFileCompiler
 
-    match "about/*.md" $ do
-        route $ setExtension "html"
-        let ctx =  constField "title" "About"
-                <> constField "aboutactive" "active"
-                <> constField "abouturl" nullLink
-                <> pageCtx
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" ctx
-            >>= relativizeUrls
+  match "about/*.md" $ do
+    route $ setExtension "html"
+    let ctx =  constField "title" "About"
+            <> constField "aboutactive" "active"
+            <> constField "abouturl" nullLink
+            <> pageCtx
+    compile $ pandocCompiler
+      >>= loadAndApplyTemplate "templates/default.html" ctx
+      >>= relativizeUrls
 
-    match "about/*.html" $ do
-        route idRoute
-        compile $ copyFileCompiler
+  match "about/*.html" $ do
+    route idRoute
+    compile copyFileCompiler
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    pageCtx
-            >>= saveSnapshot "content"
-            >>= loadAndApplyTemplate "templates/default.html" pageCtx
-            >>= relativizeUrls
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $ pandocCompiler
+      >>= loadAndApplyTemplate "templates/post.html"    pageCtx
+      >>= saveSnapshot "content"
+      >>= loadAndApplyTemplate "templates/default.html" pageCtx
+      >>= relativizeUrls
 
-    create ["posts.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let postsCtx
-              =  listField "posts" pageCtx (return posts)
-              <> constField "title" "Posts"
-              <> pageCtx
+  create ["posts.html"] $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let postsCtx =  listField "posts" pageCtx (return posts)
+                   <> constField "title" "Posts"
+                   <> pageCtx
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/posts.html" postsCtx
+        >>= loadAndApplyTemplate "templates/default.html" postsCtx
+        >>= relativizeUrls
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/posts.html" postsCtx
-                >>= loadAndApplyTemplate "templates/default.html" postsCtx
-                >>= relativizeUrls
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let indexCtx =  listField "posts" pageCtx (return posts)
+                   <> constField "title" "Home"
+                   <> constField "homeactive" "active"
+                   <> constField "homeurl" nullLink
+                   <> pageCtx
+      getResourceBody
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "templates/default.html" indexCtx
+        >>= relativizeUrls
 
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =  listField "posts" pageCtx (return posts)
-                         <> constField "title" "Home"
-                         <> constField "homeactive" "active"
-                         <> constField "homeurl" nullLink
-                         <> pageCtx
+  match "templates/*" $ compile templateCompiler
 
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
+  let feedCtx = pageCtx <> bodyField "description"
 
-    match "templates/*" $ compile templateCompiler
+  create ["atom.xml"] $ do
+    route idRoute
+    compile $ do
+      posts <- feed
+      renderAtom feedConfig feedCtx posts
 
-    create ["atom.xml"] $ do
-      route idRoute
-      compile $ do
-        let feedCtx = pageCtx <> bodyField "description"
-        posts <- feed
-        renderAtom feedConfig feedCtx posts
-
-    create ["rss.xml"] $ do
-      route idRoute
-      compile $ do
-        let feedCtx = pageCtx <> bodyField "description"
-        posts <- feed
-        renderAtom feedConfig feedCtx posts
+  create ["rss.xml"] $ do
+    route idRoute
+    compile $ do
+      let feedCtx = pageCtx <> bodyField "description"
+      posts <- feed
+      renderAtom feedConfig feedCtx posts
 
 feed = recentFirst =<<
   loadAllSnapshots "posts/*" "content"
@@ -99,11 +97,11 @@ nullLink = "javascript:void(0)"
 -- | Default setup is for individual post pages
 pageCtx :: Context String
 pageCtx = mconcat
-      [ constField "homeactive" ""
-      , constField "homeurl" "/"
-      , constField "aboutactive"  ""
-      , constField "abouturl"     "/about"
-      , constField "author" "Steven Shaw"
-      , dateField "date" "%e %B %Y"
-      , defaultContext
-      ]
+  [ constField "homeactive" ""
+  , constField "homeurl" "/"
+  , constField "aboutactive"  ""
+  , constField "abouturl"     "/about"
+  , constField "author" "Steven Shaw"
+  , dateField "date" "%e %B %Y"
+  , defaultContext
+  ]
