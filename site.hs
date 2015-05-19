debe8f3ee3b68d3d45a75270555b319fa147ca8a
Steven Shaw <steven@steshaw.org>
2015-05-19 11:01:11 +1000

    Generate posts to blog/YYYY/MM/DD/a-blog-title/index.html

--- site.hs
+++ site.hs
@@ -4,7 +4,7 @@
 import Hakyll
 import Data.Monoid
 import Data.Maybe
-import Control.Applicative
+import Control.Applicative ((<$>), empty)
 import qualified Data.ByteString.Lazy.Char8 as LB
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as E
@@ -19,10 +19,15 @@ import Data.Time.Clock (UTCTime)
 
 import System.FilePath (takeFileName)
 
+import Text.ParserCombinators.Parsec.Char
+import Text.ParserCombinators.Parsec
+
+import Debug.Trace (trace)
+
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-  
+
   everyPost <- getMatches allPosts
   let sortedPosts = sortIdentifiersByDate everyPost
 
@@ -67,16 +72,18 @@ main = hakyll $ do
     route $ constRoute ("tags/" ++ tag ++ "/index.html")
     compile $ do
       posts <- recentFirst =<< loadAll pattern
-      let ctx = constField "title" title 
-             <> listField "posts" postCtx (return posts) 
+      let ctx = constField "title" title
+             <> listField "posts" postCtx (return posts)
              <> pageCtx
-      makeItem "" 
-        >>= loadAndApplyTemplate "templates/tag.html" ctx 
-        >>= loadAndApplyTemplate "templates/default.html" ctx 
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/tag.html" ctx
+        >>= loadAndApplyTemplate "templates/default.html" ctx
         >>= relativizeUrls
 
   match allPosts $ do
-    route $ setExtension "html"
+    route $ customRoute (floop . toFilePath)
+      `composeRoutes` setExtension "html"
+      `composeRoutes` customRoute (htmlToOwnDir . toFilePath)
     compile $ pandocCompiler
       >>= loadAndApplyTemplate "templates/post.html"    postCtx
       >>= saveSnapshot "content"
@@ -116,11 +123,38 @@ main = hakyll $ do
   mkFeed "atom.xml" renderAtom
   mkFeed "rss.xml"  renderRss
 
+  where
+    -- "blog/2015/03/29/this-is-cool.html" => "blog/2015/03/29/this-is-cool/index.html"
+    htmlToOwnDir :: String -> String
+    htmlToOwnDir url = either (const url) id pr
+      where
+        pr = runParser p 0 "<blog>" url
+        p = do
+          stuff <- many $ noneOf "."
+          return $ stuff ++ "/index.html"
+    floop :: String -> String
+    floop url = trace ("url = '" ++ url ++ "'") $ either (const url) id r
+      where
+        r = trace ("result = " ++ show (parseResult url)) $ parseResult url
+    parseResult :: String -> Either ParseError String
+    parseResult = runParser parsePostUrl () "<postUrl>"
+    parsePostUrl :: Parser String
+    parsePostUrl = do
+      year <- string "posts/"
+      year <- count 4 digit
+      _ <- char '-'
+      month <- count 2 digit
+      _ <- char '-'
+      day <- count 2 digit
+      _ <- char '-'
+      title <- many anyChar
+      return $ intercalate "/" ["blog", year, month, day, title]
+
 compressJsCompiler :: Compiler (Item String)
 compressJsCompiler = fmap jasmin <$> getResourceString
 
 jasmin :: String -> String
-jasmin src = LB.unpack $ minify $ LB.fromChunks [E.encodeUtf8 $ T.pack src] 
+jasmin src = LB.unpack $ minify $ LB.fromChunks [E.encodeUtf8 $ T.pack src]
 
 allPosts = "posts/*"
 
@@ -175,43 +209,38 @@ newerPostUrl sortedPosts post = do
     let ident = itemIdentifier post
         ident' = itemBefore sortedPosts ident
     case ident' of
-        Just i -> (fmap (maybe empty $ toUrl) . getRoute) i
+        Just i -> (fmap (maybe empty toUrl) . getRoute) i
         Nothing -> empty
-
 
 olderPostUrl :: [Identifier] -> Item String -> Compiler String
 olderPostUrl sortedPosts post = do
     let ident = itemIdentifier post
         ident' = itemAfter sortedPosts ident
     case ident' of
-        Just i -> (fmap (maybe empty $ toUrl) . getRoute) i
+        Just i -> (fmap (maybe empty toUrl) . getRoute) i
         Nothing -> empty
-
 
 itemAfter :: Eq a => [a] -> a -> Maybe a
 itemAfter xs x =
     lookup x $ zip xs (tail xs)
 
-
 itemBefore :: Eq a => [a] -> a -> Maybe a
 itemBefore xs x =
     lookup x $ zip (tail xs) xs
 
-
 urlOfPost :: Item String -> Compiler String
 urlOfPost =
-    fmap (maybe empty $ toUrl) . getRoute . itemIdentifier
+    fmap (maybe empty toUrl) . getRoute . itemIdentifier
 
 --------------------------------------------------------------------------------
 -- cribbed from https://github.com/rgoulter/my-hakyll-blog/blob/master/site.hs
 --------------------------------------------------------------------------------
 
 sortIdentifiersByDate :: [Identifier] -> [Identifier]
-sortIdentifiersByDate identifiers =
-    reverse $ sortBy byDate identifiers
-        where
-            byDate id1 id2 =
-                let fn1 = takeFileName $ toFilePath id1
-                    fn2 = takeFileName $ toFilePath id2
-                    parseTime' fn = parseTime defaultTimeLocale "%Y-%m-%d" $ intercalate "-" $ take 3 $ splitAll "-" fn
-                in compare ((parseTime' fn1) :: Maybe UTCTime) ((parseTime' fn2) :: Maybe UTCTime)
+sortIdentifiersByDate = sortBy (flip byDate)
+  where
+    byDate id1 id2 =
+        let fn1 = takeFileName $ toFilePath id1
+            fn2 = takeFileName $ toFilePath id2
+            parseTime' fn = parseTime defaultTimeLocale "%Y-%m-%d" $ intercalate "-" $ take 3 $ splitAll "-" fn
+        in compare (parseTime' fn1 :: Maybe UTCTime) (parseTime' fn2 :: Maybe UTCTime)
