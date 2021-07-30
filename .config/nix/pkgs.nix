756ccd3c9501a5bf0b67a7ce4d93b16d1d1daca2
Steven Shaw <steven@steshaw.org>
2021-07-31 00:01:26 +1000

    Nix: "name" attribute no longer support by fetchGit

--- .config/nix/pkgs.nix
+++ .config/nix/pkgs.nix
@@ -3,8 +3,6 @@
 #
 import (
   builtins.fetchGit {
-    # Descriptive name to make the store path easier to identify.
-    name = "nixpkgs-unstable-2019-07-12";
     url = https://github.com/NixOS/nixpkgs-channels.git;
     # `git ls-remote https://github.com/NixOS/nixpkgs-channels nixpkgs-unstable`
     rev = "362be9608c3e0dc5216e9d1d5f5c1a5643b7f7b1";
