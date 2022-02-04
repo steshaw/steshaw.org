673e19b0f834c8c289566e0d1b928f50db4520f3
Steven Shaw <steven@steshaw.org>
2022-02-05 08:21:10 +1000

    Switch to nixpkgs from nixpkgs-channels

--- .config/nix/pkgs.nix
+++ .config/nix/pkgs.nix
@@ -3,7 +3,7 @@
 #
 import (
   builtins.fetchGit {
-    url = https://github.com/NixOS/nixpkgs-channels.git;
+    url = "https://github.com/NixOS/nixpkgs";
     # `git ls-remote https://github.com/NixOS/nixpkgs-channels nixpkgs-unstable`
     rev = "362be9608c3e0dc5216e9d1d5f5c1a5643b7f7b1";
   }
